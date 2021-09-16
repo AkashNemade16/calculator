ce74d6c52cec2a2b55a1ec62161a57669ae12dba
Akash Nemade <37910311+AkashNemade16@users.noreply.github.com>
2021-09-15 17:07:56 -0700

    number grid done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./styles.css";
 
 export default function App() {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="squares">
+        {numbers.map((i, j) => {
+          return (
+            <div key={j} className="Numbers">
+              {i}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
